523ceffb0d718fe9cacec854f5be2ca3f546834e
donfortune <donfortunet.df@gmail.com>
2023-07-14 11:00:35 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "main-instance"
+  }
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name = "ecr-repo"
+}
+
+resource "aws_iam_role" "eks_iam_role" {
+  name = "my-eks-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_iam_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_iam_role.name
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_iam_role.arn
+
+  vpc_config {
+    subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly-EKS,
+  ]
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-state-files"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "my-terraform-state-files-bucket"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0c3457caeff27567b"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-06c0e3c86a14204c1"
+ }
